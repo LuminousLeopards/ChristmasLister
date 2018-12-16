09f102cedc79c639e479523c5f89051f7e0a5eb8
Jared Peterson <hobbitcakes@gmail.com>
2018-12-16 14:01:27 -0600

    allow docker to host the app

--- christmaslister/webtier/settings.py
+++ christmaslister/webtier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's@b)lar)2d&p83efx#w5e8%55+x0$k!yv&76*50qz!k9+6j(y+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
