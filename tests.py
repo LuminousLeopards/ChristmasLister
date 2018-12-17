2998e4d9f710ac00ee6aa9583ee5c0d5b8e130fc
LuminousLeopards <LuminousLeopards@users.noreply.github.com>
2018-12-16 18:21:24 -0600

    General Cleanup
    Updates to separate tests out, clean up classes, syntax

--- tests.py
+++ tests.py
@@ -1,11 +1,23 @@
 import main,dbsetup
 
-brian = main.listuser('Brian', 'Peterson')
-#main.adduser(brian.firstname,brian.lastname)
-print(dir(brian))
-print(type(brian))
 
-print("User name: ", brian.firstame, brian.lastname)
-print(main.listuser.getuserid('Brian'))
-print(dbsetup.returntables())
+# def returntables():
+#     with dbsetup.connect:
+#         c = dbsetup.connect.cursor()
+#         for row in c.execute('SELECT rowid from users'):
+#             print(row)
 
+
+brian = main.ListUser('Brian', 'Peterson')
+main.ListUser.adduser(brian, brian.firstame, brian.lastname)
+# print(dir(brian))
+# print(type(brian))
+print("User name: ", brian.firstame, brian.lastname, ". Userid: ", brian.userid)
+
+noella = main.ListUser('Noella', 'Peterson')
+main.ListUser.adduser(noella, noella.firstame, noella.lastname)
+
+print("User name: ", noella.firstame, noella.lastname, ". Userid: ", noella.userid)
+
+#####To do : figure out workflow of how userid is associated with the list item
+#candles = main.ListItem()
